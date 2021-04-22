5b091e5f435df804cdeaf3e8e7d35d8fc4e83533
LuchnikovI <luchnikovilya@gmail.com>
2021-04-22 22:20:34 +0300

    Update natural_gradient.py

--- qgoptax/optimizers/natural_gradient.py
+++ qgoptax/optimizers/natural_gradient.py
@@ -47,10 +47,10 @@ class NaturalRGD:
         Returns:
             new parameters"""
 
-        A = lambda x: self.A(x) + eps * x
-        A = lambda x: tree_multimap(A, params, (x,))
+        A = lambda x: self.A(params, x) + eps * x
+        A = lambda x: tree_map(A, x)
         rgrad = cg(A, grads, x0=x0, tol=tol, atol=atol, maxiter=maxiter, M=M)
         rgrad = tree_map(lambda x: self.manifold.proj(params, x), rgrad)
-        params = tree_multimap(lambda x, y: x - self.learning_rate * y, params, (rgrad,))
+        params = tree_multimap(lambda x, y: x - self.learning_rate * y, params, rgrad)
         return params
 
