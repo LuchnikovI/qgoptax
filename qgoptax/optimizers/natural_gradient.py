335431344c808d080cb4a0b28e03c5bdc4e6f319
LuchnikovI <luchnikovilya@gmail.com>
2021-04-23 18:41:55 +0300

    Update natural_gradient.py

--- qgoptax/optimizers/natural_gradient.py
+++ qgoptax/optimizers/natural_gradient.py
@@ -11,12 +11,15 @@ class NaturalRGD:
                  dist: Callable[[Params, Params], jnp.ndarray],
                  learning_rate: jnp.ndarray,
                  name='NaturalRGD',
-                 penalty=1e6):
+                 orth_penalty=1e6,
+                 penalty=1e-2):
         self.manifold = manifold
         self.name = name
-        metric = lambda u, v: jvp(grad(lambda x: dist(u, x)), (u,), (v,))[1]
-        orthogonal_penalty = lambda u, v: tree_multimap(lambda x, y: penalty*(y - manifold.proj(x, y)), u, v)
-        self.A = lambda u, v: tree_multimap(lambda x, y: x + y, tree_multimap(manifold.proj, u, metric(u, tree_multimap(manifold.proj, u, v))), orthogonal_penalty(u, v))
+        self.metric_action = lambda u, v: jvp(grad(lambda x: dist(u, x)), (u,), (v,))
+        self.total_penalty = lambda x, y: tree_multimap(lambda u, v: orth_penalty * (v - manifold.proj(u, v)) + penalty * v, x, y)
+        self.tree_proj = lambda u, v: tree_multimap(manifold.proj, u, v)
+        self.A = lambda u, v: self.tree_proj(u, self.metric_action(u, self.tree_proj(u, v)))
+        self.penalized_A = lambda u, v: tree_multimap(lambda x, y: x+y, self.A(u, v), self.total_penalty(u, v))
         self.learning_rate = learning_rate
         
 
@@ -29,7 +32,6 @@ class NaturalRGD:
 
     def update(self, grads: Params,
                params: Params,
-               eps=1e-2,
                x0=None,
                tol=1e-05,
                atol=0.0,
@@ -49,10 +51,8 @@ class NaturalRGD:
         Returns:
             new parameters"""
 
-        A = lambda x: tree_multimap(lambda a, b: a + eps * b, self.A(params, x), x)
-        preprocessing = lambda x: tree_multimap(lambda u, v: self.manifold.proj(u, v.conj()), params, x)
-        proj_grads = preprocessing(grads)
+        A = lambda v: self.penalized_A(params, v)
+        proj_grads = lambda x: tree_multimap(lambda u, v: self.manifold.proj(u, v.conj()), params, x)(grads)
         rgrad = cg(A, proj_grads, x0=x0, tol=tol, atol=atol, maxiter=maxiter, M=M)[0]
-        rgrad = tree_multimap(lambda x, y: self.manifold.proj(x, y), params, rgrad)
         params = tree_multimap(lambda x, y: self.manifold.retraction(x, -self.learning_rate*y), params, rgrad)
         return params
